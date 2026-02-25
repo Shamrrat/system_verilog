f46f77082ff24014c5e11356480d7eef835b309b
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2026-02-25 17:32:05 +0530

    Fix module name and add reset assertion

--- day11/assertion/simple_assertion.sv
+++ day11/assertion/simple_assertion.sv
@@ -1,1 +1,11 @@
-module asssertioon;
+module test;
+  logic clk;
+  logic rst;
+  logic [3:0] counter;
+
+  always @(posedge clk) begin
+    if (rst)
+      assert(counter == 0)
+        else $error("Counter not zero during reset!");
+  end
+endmodule
